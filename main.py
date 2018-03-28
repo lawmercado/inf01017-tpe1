2a2cfb5907a9ef6f9ac0a408be586f9566452aa7
Mint <gabrijob@gmail.com>
2018-03-28 02:10:54 -0300

    mild statistics calculations

--- main.py
+++ main.py
@@ -25,6 +25,25 @@ for idx, fold in enumerate(folds):
 
     print(classified)
 
-    # Compare classified instances with the training set
-    # Then generate the statistics
+    # Compare classified instances with the test set
+    accuracy = 0
+    true_positive = 0
+    false_positive = 0
+    false_negative = 0
+    for (predicted_instance, test_instance) in zip(classified, test_instances):
+        if predicted_instance[1] == test_instance[1]:
+           accuracy += 1
+           if predicted_instance[1] == 1:
+               true_positive += 1
+        elif predicted_instance[1] == 1:
+            false_positive += 1
+        elif predicted_instance[1] == 0:
+            false_negative += 1
 
+    # Then generate the statistics
+    accuracy = accuracy / len(classified)  # vai dar atribuir int ou float?
+    rev = true_positive / (true_positive + false_negative)
+    prec = true_positive / (true_positive + false_positive)
+    f_measure = 2 * (prec * rev) / (prec + rev)
+
+    # Save statistics (list de tuplas?)
